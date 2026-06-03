48260397100e6d3cd5c4bc6c9acdae6d0484e8da
bhawani mokawat <bhawanimokawat599@gmail.com>
2026-06-03 09:16:35 +0530

    Fix Navbar import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
  import { useState } from "react";
- import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
  function App() {
 
   const [todo, setTodo] = useState("")
